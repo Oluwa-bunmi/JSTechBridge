459e105193cf09de539c5d2da4beedfc15cd83bc
Jummy007 <gbemisoyeadejumoke@gmail.com>
2024-06-07 21:55:31 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// declare variables with let, const or var
+
+let career = "Frontend Development";
+let student = "Mr Taiwo";
+// string concatenation
+console.log(student + " is studying " + career + " and he is two years old ");
+// string interpolation using template literals
+console.log(`${student} is studying ${career} and he is two years old`);
+console.log(career.length);
+console.log(career[0]); // to get a particular string with its index
+
+// Another example of string interpolation
+var color = "blue";
+var age = 24;
+var gender = "male";
+// he is a male of age 24 and his best color is blue
+console.log(`he is a ${gender} of age ${age} and his best color is ${color}`);
+
+// data type number
+let numOne = 2;
+let numTwo = 3;
+console.log(numOne * numTwo);
+
+// data type boolean i.e true or false
+console.log("2" == 2);
+console.log(typeof 2);
+// console.log(typeof(2));
+console.log(typeof false);
+
+// == or === means equality in js
